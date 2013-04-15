c67071f25013b042b1bcf76eb202910fc91cddf9
Seán Dunne <sean.dunne1988@gmail.com>
2013-04-15 23:09:44 +0100

    Added locking to server Impl

--- code/suncertify/server/DataServiceImpl.java
+++ code/suncertify/server/DataServiceImpl.java
@@ -13,17 +13,25 @@ public class DataServiceImpl implements DataService {
 
 	@Override
 	public String[] read(final int recNo) throws RecordNotFoundException, RemoteException {
-		return this.data.read(recNo);
+		this.data.lock(recNo);
+		String[] record = this.data.read(recNo);
+		this.data.unlock(recNo);
+
+		return record;
 	}
 
 	@Override
 	public void update(final int recNo, final String[] data) throws RecordNotFoundException, RemoteException {
+		this.data.lock(recNo);
 		this.data.update(recNo, data);
+		this.data.unlock(recNo);
 	}
 
 	@Override
 	public void delete(final int recNo) throws RecordNotFoundException, RemoteException {
+		this.data.lock(recNo);
 		this.data.delete(recNo);
+		this.data.unlock(recNo);
 	}
 
 	@Override
