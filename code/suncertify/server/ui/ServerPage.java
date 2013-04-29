0475bbfca4b92732d373ba459d7a300d2cbb527f
Seán Dunne <sean.dunne@ericsson.com>
2013-04-29 15:05:04 +0100

    Added path to db file to server UI

--- code/suncertify/server/ui/ServerPage.java
+++ code/suncertify/server/ui/ServerPage.java
@@ -1,16 +1,28 @@
 package suncertify.server.ui;
 
+import static suncertify.db.ServerFactory.DB_LOCATION;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import suncertify.shared.Preferences;
 
 public class ServerPage extends JPanel {
 
 	private static final long serialVersionUID = 5317757024984525594L;
 
 	public ServerPage() {
+		JLabel dbFileLocLbl = new JLabel("Database file: ");
+		JTextField dbFileLocTxt = new JTextField(Preferences.getInstance().get(DB_LOCATION));
+
+		this.add(dbFileLocLbl);
+		this.add(dbFileLocTxt);
+
 		JButton b = new JButton("Shutdown");
 		b.addActionListener(new ActionListener() {
 			@Override
